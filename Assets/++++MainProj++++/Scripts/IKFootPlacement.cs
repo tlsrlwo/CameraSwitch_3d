bd2f669fbe51fcb2d190d23c9f148f053d0b2c8d
DESKTOP\gijae <liongijae@naver.com>
2024-09-02 18:25:01 +0900

    __

--- Assets/++++MainProj++++/Scripts/IKFootPlacement.cs
+++ Assets/++++MainProj++++/Scripts/IKFootPlacement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,46 @@ namespace CST
 {
     public class IKFootPlacement : MonoBehaviour
     {
+        Animator anim;
+        public LayerMask layerMask;
         
+        [Range(0, 1f)]
+        public float DistanceToGround;
+
+
+        private void Awake()
+        {
+            anim = GetComponentInChildren<Animator>();
+           
+        }
+        private void Update()
+        {
+            
+        }
+
+        private void OnAnimatorIK(int layerIndex)
+        {
+            if(anim)
+            {
+                // only works for humanoid avatars
+                anim.SetIKPositionWeight(AvatarIKGoal.LeftFoot, 1f);
+                anim.SetIKRotationWeight(AvatarIKGoal.LeftFoot, 1f);
+
+                // Left Foot
+                RaycastHit hit;
+                Ray ray = new Ray(anim.GetIKPosition(AvatarIKGoal.LeftFoot) + Vector3.up, Vector3.down);
+                if(Physics.Raycast(ray, out hit, DistanceToGround + 1f))
+                {
+                    if(hit.transform.tag == "Walkable")
+                    {
+                        Vector3 footPosition = hit.point; //hit.transform.point 를 하면 hit 한 물체의 transform 지점이라 rayHit 지점이랑 다름
+                        footPosition.y += DistanceToGround;
+                        anim.SetIKPosition(AvatarIKGoal.LeftFoot, footPosition);
+                    }
+                }
+
+            }
+        }
+
     }
 }
---
+++ Assets/++++MainProj++++/Scripts/Interaction/NPCInteractable.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CST
+{
+    public class NPCInteractable : MonoBehaviour
+    {
+      public void Interact()
+        {
+            Debug.Log("Interact!");
+        }
+    }
+}
